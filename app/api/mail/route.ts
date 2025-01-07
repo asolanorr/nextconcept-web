a7e800a9359a9f04f5fe6b0e74c9920d46706b74
Alejandro Solano <alejandrosolanorodriguez@gmail.com>
2025-01-07 13:21:12 -0600

    Fix: unused variable

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: Request, response: Response) {
+export async function POST(request: Request) {
   const { fullName, company, email, phone, message } = await request.json();
 
   try {
