b676ca1cf0bd59c1fc5f13f5339a0cf885c50e29
Alejandro Solano <alejandrosolanorodriguez@gmail.com>
2025-01-08 16:16:50 -0600

    Fix: minimal build issues fixed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,18 @@
-import styles from '@/app/_components/FooterSection/footerSeccion.module.css';
+import styles from '@/components/FooterSection/footerSeccion.module.css';
 import Navbar from '@/components/navbar/navbar';
 
 export default function About() {
   return (
     <div className={styles.page}>
       <Navbar />
-      <h1>About Us</h1>
-      <h1>About Us</h1>
-      <h1>About Us</h1>
-      <h1>About Us</h1>
-      <h1>About Us</h1>
-      <h1>About Us</h1>
-      <h1>About Us</h1>
-      <h1>About Us</h1>
+      <h1>Working on it...</h1>
+      <h1>Working on it...</h1>
+      <h1>Working on it...</h1>
+      <h1>Working on it...</h1>
+      <h1>Working on it...</h1>
+      <h1>Working on it...</h1>
+      <h1>Working on it...</h1>
+      <h1>Working on it...</h1>
     </div>
   );
 }
