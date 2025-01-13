59b258c7e980fece89b538c5c64e6bccceb43f09
Alejandro Solano <alejandrosolanorodriguez@gmail.com>
2025-01-12 23:50:04 -0600

    SEO: Basic SEO added to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,50 @@ const lato = Lato({
 });
 
 export const metadata: Metadata = {
-  title: 'Next Concept',
-  description: 'Bring Your Next Concept to Life.',
+  metadataBase: new URL('https://yournextconcept.com'),
+  keywords: [
+    'Software development services',
+    'Outsourcing software development',
+    'Custom software solutions',
+    'Web and mobile app development',
+    'Professional software developers',
+    'Next Concept software outsourcing',
+    'Bring your concept to life',
+    'High-quality software solutions',
+    'Full-stack development outsourcing',
+    'Tailored software development Costa Rica',
+    'Affordable software development',
+    'Front-end and UI development services',
+    'UX/UI design and coding services',
+    'Robust web application development',
+    'Cost-effective software development',
+    'React and Vue development experts',
+    'Next.js and TypeScript projects',
+    'Custom .NET backend solutions',
+    'Full-stack app development company',
+    'Software development services in Costa Rica',
+    'Latin America software outsourcing',
+    'Nearshore development company',
+  ],
+  title: 'Next Concept - Bring Your Concept to Life',
+  description:
+    'Next Concept offers high-quality, custom software development services. From front-end design to robust web applications, we bring your ideas to life with tailored solutions.',
+  openGraph: {
+    title: 'Next Concept - Bring Your Concept to Life',
+    description:
+      'Next Concept offers high-quality, custom software development services. From front-end design to robust web applications, we bring your ideas to life with tailored solutions.',
+    url: 'https://yournextconcept.com',
+    type: 'website',
+    images: [
+      {
+        url: 'https://yournextconcept.com/og-image.jpg', // Replace with your actual image URL
+        width: 1200,
+        height: 630,
+        alt: 'Next Concept Logo and Slogan',
+      },
+    ],
+    siteName: 'Next Concept',
+  },
 };
 
 export default function RootLayout({
---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://yournextconcept.com',
+      lastModified: new Date(),
+    },
+  ];
+}
